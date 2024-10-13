d025fb513e30054c295fd805f368fa21d4702e26
Snehagupta1907 <snehagupta98930@gmail.com>
2024-10-13 14:42:50 +0530

    1v1 page added

---
+++ client/src/app/1v1/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const OneVOne = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-[100vh] bg-dark-3">
+        <div
+        
+        >
+          <div id="YakındaYazı" className="text-p1 text-3xl space-y-3">
+            █ █ █ █ █ █<span style={{color:"black"}}> █ █ █ █ █ █ █ </span>51%
+            <br className="mt-3" />
+            &gt; One V One is Coming Soon ...
+            
+            <br className="mt-3" />
+            &gt; Buzzi-Fy Fetching...<span id="imleç">█</span>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OneVOne;
