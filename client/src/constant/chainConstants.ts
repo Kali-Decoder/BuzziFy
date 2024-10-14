ff60bada04544547eaebf16e7ab7186ff439860c
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-10-14 19:19:48 +0530

    Update base Sepolia:

--- client/src/constant/chainConstants.ts
+++ client/src/constant/chainConstants.ts
@@ -1,22 +1,6 @@
-import { sepolia, polygonAmoy, baseSepolia, holesky } from "@wagmi/core/chains";
+import { baseSepolia } from "@wagmi/core/chains";
 import { http } from "viem";
-import { KiiTestnet } from "./chain";
-export const chainArray = [
-  sepolia,
-  {
-    ...polygonAmoy,
-    iconUrl:
-      "https://streamnft-chain.s3.ap-south-1.amazonaws.com/polygonLogo.png",
-  },
-  baseSepolia,
-  KiiTestnet,
-  holesky,
-];
-
+export const chainArray = [baseSepolia];
 export const transportsObject = {
-  [sepolia.id]: http(),
-  [polygonAmoy.id]: http(),
   [baseSepolia.id]: http(),
-  [KiiTestnet.id]: http(),
-  [holesky.id]: http(),
 };
